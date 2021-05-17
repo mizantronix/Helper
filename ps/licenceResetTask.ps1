da28bd3e1218fc79e9f2313b2d52e0127c1d0eb1
andrey <mizantronix@gmail.com>
2021-05-17 17:40:23 +0700

    Resharper & Reflector

---
+++ ps/licenceResetTask.ps1
@@ -0,0 +1,21 @@
+$trigger = New-JobTrigger -DaysInterval 30 -Daily -At 10:00
+
+# Resharper
+
+Register-ScheduledJob -Name "Clear resharper licence" -Trigger $trigger -ScriptBlock {
+  if (Test-Path get-item -Path Registry::HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Ext\Settings\) { 
+    Remove-Item Registry::HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Ext\Settings\ -Recurse 
+    }
+  }
+
+(Get-ScheduledJob -Name "Clear resharper licence").StartJob()
+
+
+# .Net Reflector
+Register-ScheduledJob -Name "Clear .Net Reflector licence" -Trigger $trigger -ScriptBlock {
+  if (Test-Path get-item -Path Registry::HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Ext\Settings\) { 
+    Remove-Item Registry::HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Ext\Settings\ -Recurse 
+    }
+  }
+
+(Get-ScheduledJob -Name "Clear .Net Reflector licence").StartJob()
