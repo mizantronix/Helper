abbedd041f3297d4ae42d32fdc326f3b54dbb473
mizantronix <mizantronix@gmail.com>
2022-09-25 17:56:39 +0700

    Update Initializer.ps1

--- scripts/ps/Initializer.ps1
+++ scripts/ps/Initializer.ps1
@@ -1,31 +1,5 @@
-Param(
-    [switch]
-    $additionalSoft = $false
-)
-
 Set-ExecutionPolicy Bypass -Scope Process -Force
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-choco install vscode -y
-choco install git -y
-choco install gitextensions -y
-choco install kdiff3 -y
-choco install nugetpackageexplorer -y
-choco install notepadplusplus -y
-choco install fiddler -y
-choco install far -y
-choco install wsl -y
-choco install putty -y
-choco install nodejs -y
-choco install tightvnc -y
-choco install dotnet-sdk -y
-choco install yandexdisk -y
-
-if ($additionalSoft)
-{
-    choco install steam-client -y
-    choco install telegram.install -y
-    choco install origin -y
-    choco install battle.net -y
-}
+choco install vscode git gitextensions kdiff3 nugetpackageexplorer notepadplusplus fiddler far wsl putty nodejs tightvnc dotnet-sdk yandexdisk steam-client telegram.install -y
