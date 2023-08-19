a51e8ec7f6846f41428f39f669ce014c44a9f8e0
mizantronix <mizantronix@gmail.com>
2023-08-20 01:23:53 +0700

    Update Initializer.ps1

--- scripts/ps/Initializer.ps1
+++ scripts/ps/Initializer.ps1
@@ -2,4 +2,4 @@ Set-ExecutionPolicy Bypass -Scope Process -Force
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-choco install vscode git gitextensions kdiff3 nugetpackageexplorer notepadplusplus fiddler far wsl putty nodejs tightvnc dotnet-sdk yandexdisk windirstat steam-client telegram.install winscp msys2 -y
+choco install vscode git gitextensions kdiff3 nugetpackageexplorer notepadplusplus fiddler far wsl putty nodejs 7zip.install tightvnc dotnet-sdk yandexdisk windirstat steam-client telegram.install winscp msys2 -y
