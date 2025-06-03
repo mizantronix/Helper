0d28a4ab9a910d085728e9e9eb227fdf0205465c
a.guliganov <a.guliganov@loymax.local>
2025-06-03 17:47:43 +0700

    old context menu

--- scripts/ps/Initializer.ps1
+++ scripts/ps/Initializer.ps1
@@ -3,3 +3,9 @@ Set-ExecutionPolicy Bypass -Scope Process -Force
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 choco install vscode git gitextensions kdiff3 nugetpackageexplorer notepadplusplus fiddler far wsl putty nodejs 7zip.install tightvnc dotnet-sdk yandexdisk windirstat steam-client telegram.install winscp msys2 -y
+
+New-Item -Path "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}" -Force | Out-Null
+New-Item -Path "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" -Force | Out-Null
+Set-ItemProperty -Path "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" -Name "(default)" -Value "" 
+Stop-Process -Name explorer -Force
+Start-Process explorer
