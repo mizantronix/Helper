4c2348e042238f6a4911083ea39e113e4c4dfc1d
mizantronix <mizantronix@gmail.com>
2026-01-23 15:53:09 +0700

    Update userprofile-additions.ps1

--- scripts/ps/userprofile-additions.ps1
+++ scripts/ps/userprofile-additions.ps1
@@ -39,3 +39,30 @@ function Clean-Offset {
   [void](Invoke-WebRequest "$debeziumUrl/connectors/$connectorName/resume" -Method PUT -UseBasicParsing)
   Write-Host "Done"
 }
+
+function Delete-Connector {
+  <#
+  .SYNOPSIS
+    Delete connector — stopping connector, delete offsets, delete connector
+  .PARAMETER debeziumUrl
+    URL for debezium API; '***' for DEV env
+  .PARAMETER connectorName
+    connector name, witch offset must be cleaned
+  #>
+  param (
+    [Parameter(Mandatory = $true, HelpMessage = "DEV - '***'`nQA - '***'`n")]
+    [string] $debeziumUrl,
+    [Parameter(Mandatory = $true)]
+    [string] $connectorName
+  )
+
+  Write-Host "Stopping connector..." -NoNewline
+  [void](Invoke-WebRequest "$debeziumUrl/connectors/$connectorName/stop" -Method PUT -UseBasicParsing)
+  Write-Host "Done"
+  Write-Host "Deleting offsets..." -NoNewline
+  [void](Invoke-WebRequest "$debeziumUrl/connectors/$connectorName/offsets" -Method DELETE -UseBasicParsing)
+  Write-Host "Done"
+  Write-Host "Deleting connector..." -NoNewline
+  [void](Invoke-WebRequest "$debeziumUrl/connectors/$connectorName/" -Method DELETE -UseBasicParsing)
+  Write-Host "Done"
+}
